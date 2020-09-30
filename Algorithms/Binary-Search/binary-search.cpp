0c271d10ce4a7da7be5ee15cbd30169d638b5381
Firas Harb <45987326+firasharb@users.noreply.github.com>
2020-09-30 21:14:43 +0300

    Update binary-search.cpp

--- Algorithms/Binary-Search/binary-search.cpp
+++ Algorithms/Binary-Search/binary-search.cpp
@@ -1,34 +1,54 @@
-// C++ program to implement recursive Binary Search 
+/* 
+  Author: Firas Harb
+  Language: GNU C++11
+  Date: Wednesday, 30 September 2020
+*/
 #include <iostream>
 #include <vector>
 
-#define FOR(i, s, n) for(int i = s; i < n; i++)
 using namespace std;
 
+//In this algorithm I've used Vectors as I feel they are the best for the job, but this could be easily
+//altered to use array or any other data structure that works.
 bool binarySearch(vector<int> arr, int l, int r, int x) 
-{ 
+{
+  //Checking if there is more than one element in the array
   if (r >= l) { 
+    //calculating the middle element in the array
     int mid = l + (r - l) / 2;
     if (arr[mid] == x) 
       return true;  
+    //recursing on left half to check for element
     if (arr[mid] > x) 
       return binarySearch(arr, l, mid - 1, x);
-    return binarySearch(arr, mid + 1, r, x); 
+    //else we recurse on the right half of the array
+    else return binarySearch(arr, mid + 1, r, x); 
   }  
+  //If all else fails return false.
   return false; 
 } 
 
+//This function is used to clean up the code, since it require of the user to just input the 
+//Array and the Element and it handles the rest.
 bool search(vector<int> arr, int elem) { 
   if(arr.size() == 0) return false;
   else return binarySearch(arr, 0, arr.size() - 1, elem);
 }
   
-int main(void) 
-{ 
+int main() 
+{
+    ///////////////////////
+   //// Start Testing ////
+  ///////////////////////
+  
   vector<int> arr;
-  FOR(i, 0, 10) arr.push_back(i); 
+  for(int i = 0; i < arr.size()-1; i++) arr.push_back(i); 
 
   cout << search(arr, 8) << "\n"; //--> returns True
   cout << search(arr, 20) << "\n"; //--> returns False
   cout << search(arr, 1623) << "\n"; //--> returns False
+  
+    /////////////////////
+   //// End Testing ////
+  /////////////////////
 }
